96aa596dad7b1b5094d0b9ccb3b89e4378126a59
Rafa <rafaelasimoes2203@mail.com>
2023-08-16 14:23:54 -0300

    alteração mensagem

--- lib/main.dart
+++ lib/main.dart
@@ -28,14 +28,14 @@ class _MyHomePageState extends State<MyHomePage> {
     Future.delayed(_interval, () {
       setState(() {
         _secondsElapsed++;
-        if (_secondsElapsed == 20) {
+        if (_secondsElapsed == 2) {
           _backgroundColor = Colors.yellow;
-        } else if (_secondsElapsed == 40) {
+        } else if (_secondsElapsed == 4) {
           _backgroundColor = Colors.orange;
-        } else if (_secondsElapsed == 60) {
+        } else if (_secondsElapsed == 6) {
           _backgroundColor = Colors.red;
           _message = 'Cada minuto dedicado ao aprendizado e crescimento pessoal é um passo na direção dos seus sonhos. Então, que tal aproveitar cada momento para construir um futuro incrível?';
-        } else if (_secondsElapsed == 75) {
+        } else if (_secondsElapsed == 10) {
           _exitApp();
           return;
         }
@@ -77,9 +77,13 @@ class _MyHomePageState extends State<MyHomePage> {
     return Scaffold(
       backgroundColor: _backgroundColor,
       body: Center(
-        child: Text(
-          _message,
-          style: TextStyle(fontSize: 24, color: Colors.white),
+        child: Container(
+          width: MediaQuery.of(context).size.width * 0.75, // 75% da largura da tela
+          child: Text(
+            _message,
+            style: TextStyle(fontSize: 24, color: Colors.white),
+            textAlign: TextAlign.center,
+          ),
         ),
       ),
     );
